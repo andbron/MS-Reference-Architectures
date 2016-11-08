0f8ec5e457a28922ab4220328c56359298b30f47
Hanz Zhang <hanz@microsoft.com>
2016-11-08 14:10:00 -0800

    add domain join code using powershell

---
+++ guidance-identity-adfs/extensions/domain-join1.ps1
@@ -0,0 +1,16 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True)]
+  [string]$AdminUser,
+
+  [Parameter(Mandatory=$True)]
+  [string]$AdminPassword,
+
+  [Parameter(Mandatory=$True)]
+  [string]$DomainName
+)
+#  $DomainName = "contoso.com"
+$secAdminPassword = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ("$DomainName\$AdminUser", $secAdminPassword)
+Add-Computer -DomainName $DomainName -Credential $credential
+Restart-Computer
