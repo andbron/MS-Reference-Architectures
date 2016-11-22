960d24302fa85ef9b665602fdab502ab39118df7
RohitSharma-pnp <roshar@microsoft.com>
2016-11-22 15:57:45 -0800

    updated n-tier sql RA to support keyvault

--- guidance-compute-single-vm/Deploy-ReferenceArchitecture.ps1
+++ guidance-compute-single-vm/Deploy-ReferenceArchitecture.ps1
@@ -5,8 +5,8 @@
 param(
   [Parameter(Mandatory=$true)]
   $SubscriptionId,
-  [Parameter(Mandatory=$true)]
-  $ResourceGroupName,
+  [Parameter(Mandatory=$false)]
+  $ResourceGroupName = "ra-single-vm-rg",
   [Parameter(Mandatory=$false)]
   $Location = "Central US",
   [Parameter(Mandatory=$false)]
